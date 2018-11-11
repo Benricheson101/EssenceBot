18f88b76e980eb20392bdb5cb5610b9fad1d57d9
Benricheson101 <benricheson101@gmail.com>
2018-11-11 14:16:32 -0500

    Updated User Info Command!

--- Essence/Modules/Moderation/Info.cs
+++ Essence/Modules/Moderation/Info.cs
@@ -1,27 +1,38 @@
 using System;
-using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Transactions;
 using Discord;
 using Discord.Commands;
 
 namespace Essence.Modules.Moderation
 {
-  public class UserInfo : ModuleBase<SocketCommandContext>
+  public class Info : ModuleBase<SocketCommandContext>
   {
-    [Command("user"), Alias("userinfo", "about", "who")]
+    [Command("user"), Alias("userinfo", "about", "aboutuser", "who", "whois", "uinfo", "uabout")]
     public async Task UserInformation(IGuildUser user = null)
     {
+      Console.WriteLine($"User {Context.User.Username} ran the command: {Context.Message.Content}");
+      
       if (user == null)
         user = (IGuildUser) Context.User;
         
-      await Context.Message.DeleteAsync();
-
       var joinDate = user.JoinedAt.Value.Date.Month + "/" + user.JoinedAt.Value.Date.Day + "/" + user.JoinedAt.Value.Date.Year;
       var joinTime2 = user.JoinedAt.Value.Hour + ":" + user.JoinedAt.Value.Minute;
-      //DateTime joinTime = new DateTime();
-      
-      //joinTime = joinTime.ToString()
+      var nickname = user.Nickname;
+      if (user.Nickname == null)
+        nickname = "Null";
+      var serverOwner = user.Guild.GetOwnerAsync().Result.Id;
+      bool isServerOwner = serverOwner == user.Id;
+      var currentVoiceChannel = user.VoiceChannel;
+      string inVC;
+      if (user.VoiceChannel == null)
+        inVC = "Null";
+      else
+        inVC = user.VoiceChannel.Name;
+
+
+      Console.WriteLine("Creating Embed");
 
       var ebUserInfo = new EmbedBuilder()
         .WithTitle("Information on: " + user.Username)
@@ -29,25 +40,29 @@ namespace Essence.Modules.Moderation
 
         .AddField("Username:", user.Username, true)
         .AddField("Discriminator:", user.Discriminator, true)
-        .AddField("Nickname:", user.Nickname, true)
-
+        .AddField("Nickname:", nickname, true)
 
         .AddField("User ID:", user.Id, true)
-        .AddField("Highest Role:", Context.Guild.GetRole(user.RoleIds.LastOrDefault()).Mention, true)
         .AddField("Avatar URL:",
           $"[256px]({user.GetAvatarUrl(ImageFormat.Png, 256)}) [512px]({user.GetAvatarUrl(ImageFormat.Png, 512)}) [1024px]({user.GetAvatarUrl(ImageFormat.Png, 1024)})",
           true)
-
         .AddField("Join Date:", joinDate + " at " + joinTime2, true)
+
+        .AddField("Server Owner:", isServerOwner, true)        
+        .AddField("Highest Role:", Context.Guild.GetRole(user.RoleIds.LastOrDefault()).Mention, true)
+        .AddField("Created Account:", user.CreatedAt.Date.Month + "/" + user.CreatedAt.Day + "/" + user.CreatedAt.Year, true)
+          
+        .AddField("In Voice Channel:", inVC, true)
         .AddField("Deafened:", user.IsDeafened || user.IsSelfDeafened, true)
         .AddField("Muted:", user.IsMuted || user.IsSelfMuted, true)
-
+          
         .AddField("Status:", user.Status, true)
         .AddField("Status Type:", user.Activity.Type, true)
-        .AddField("Playing Status:", user.Activity, true)
+        .AddField("Playing Status:", user.Activity, true);
 
-        .AddField("Created Account:", user.CreatedAt.Date, true);
-              
+
+      Console.WriteLine("Sending Embed");
+      
       var msg = await ReplyAsync("", embed: ebUserInfo.Build());
     }
   }
