831452c131ac2f6b9877fac871899e0363c80934
Benricheson101 <benricheson101@gmail.com>
2018-11-14 17:37:44 -0500

    Something broke with the UserInfo command...so I will keep working on that. I am going to start more important commands, like kick, ban, mute and warn.

---
+++ Essence/Modules/Moderation/Kick.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+
+namespace Essence.Modules.Moderation
+{
+  public class Kick : ModuleBase<SocketCommandContext>
+  {
+    
+  }
+}
